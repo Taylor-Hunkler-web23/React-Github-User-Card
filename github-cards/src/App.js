bd41f503e4c61bbc03cbfa14752d283682c8cfb5
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-01 15:29:53 -0400

    converted app.js into a class component, console logging data from api

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
+class App extends React.Component {
+ constructor () {
+super();
+
+
+ }
+
+
+ componentDidMount () {
+   axios
+   .get('https://api.github.com/users/taylorhh5')
+   .then(response => console.log (response))
+ }
+
+ 
+
+
+render () {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<h1>Github User Cards</h1>
     </div>
   );
+}
 }
 
 export default App;
